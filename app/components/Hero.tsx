b8a2a4f74515596e9f3f04dd10fd65b221e868af
Billal <billalpatel.bp@gmail.com>
2023-10-07 15:16:39 +0100

    Hero carousel

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,9 +9,18 @@ import lanternImage from "../../assets/islamic-lantern-svgrepo-com.svg";
 import { Button } from "./Button";
 export function Hero() {
   return (
-    <div className="hero | flex flex-row justify-between p-12 space-y-10 w-3/4 mx-auto mt-10 mb-36">
-      <Carousel autoplay autoplayInterval={1000} wrapAround={true} pauseOnHover animation={'fade'}>
-        <div className="carousel-one | w-2/4">
+    // <div className="hero | flex flex-row justify-between p-12 space-y-10 w-3/4 mx-auto mt-10 mb-36">
+    <div className="hero | flex flex-row justify-between p-12 space-y-10 mx-auto mt-10 mb-36">
+      <Carousel
+        autoplay
+        autoplayInterval={3000}
+        wrapAround={true}
+        animation={"fade"}
+        renderCenterLeftControls={null}
+        renderCenterRightControls={null}
+        renderBottomCenterControls={null}
+      >
+        <div className="carousel-one | w-2/4 mx-auto">
           <div className="space-y-6">
             <h1 className="header | text-6xl text-black">
               Aal-Imran Quran School
@@ -30,9 +39,9 @@ export function Hero() {
             className="lantern | relative -top-16 left-56 w-1/4 h-48"
           />
         </div>
-        <div className="carousel-two | w-2/4 flex flex-row justify-start text-right	">
-          <div className="flex flex-col self-end space-y-6">
-            <h1 className="header | text-8xl text-black">
+        <div className="carousel-two | w-2/4 mx-auto">
+          <div className="space-y-6 text-right">
+            <h1 className="header | text-8xl text-black text-right">
               مدرسة آل عمران القرآنية
             </h1>
             <p className="desc | text-3xl text-black">
@@ -40,9 +49,9 @@ export function Hero() {
               من مصر.
             </p>
           </div>
-          {/* <div className="my-14">
+          <div className="text-right my-14">
             <Button buttonText="اكتشف المزيد" />
-          </div> */}
+          </div>
           {/* <Image
             src={lanternImage}
             alt={""}
