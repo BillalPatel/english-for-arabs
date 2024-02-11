60aeee7d0ff6397be580591601198fa23b8dfb1e
Billal <billalpatel.bp@gmail.com>
2024-02-11 18:24:20 +0000

    Logo

--- app/components/Form/ContactForm.tsx
+++ app/components/Form/ContactForm.tsx
@@ -23,18 +23,18 @@ export default function ContactForm() {
   const onSubmit: SubmitHandler<Inputs> = function (data) {
     console.log(data);
 
-    emailjs
-      .sendForm("service_yuh4t7w", "template_6js6ypl", form.current, {
-        publicKey: "pNxkKYMTbhrNse7Hq",
-      })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
+    // emailjs
+    //   .sendForm("service_yuh4t7w", "template_6js6ypl", form.current, {
+    //     publicKey: "pNxkKYMTbhrNse7Hq",
+    //   })
+    //   .then(
+    //     () => {
+    //       console.log("SUCCESS!");
+    //     },
+    //     (error) => {
+    //       console.log("FAILED...", error.text);
+    //     }
+    //   );
   };
 
   return (
