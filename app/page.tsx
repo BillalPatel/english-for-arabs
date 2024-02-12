8c2f543cb3b09de6642ce936f010dec8b8c5fbca
Billal <billalpatel.bp@gmail.com>
2024-02-12 10:12:05 +0000

    More styling on hero

--- app/page.tsx
+++ app/page.tsx
@@ -65,18 +65,18 @@ export default function Home() {
           className="h-screen w-full bg-contain z-30 object-cover"
         />
         <div className="absolute top-0 bottom-0 left-0 right-0 flex justify-items-center items-center place-items-center place-content-center flex-col space-y-8">
-          <div className="flex flex-row w-9/12">
-            <div className="w-2/4 flex align-middle flex-col place-self-center space-y-7 text-white">
-              <h1 className="text-4xl lg:text-6xl text-left w-3/5 lg:w-full font-bold space-y-3">
+          <div className="flex flex-row w-full md:w-9/12 lg:w-9/12">
+            <div className="w-full md:w-2/4 lg:w-2/4 flex align-middle flex-col place-self-center space-y-7 text-white">
+              <h1 className="text-6xl md:text-5xl lg:text-6xl md:text-left lg:text-left text-center w-full font-bold space-y-3">
                 <span className="bg-brown py-2 px-3 text-blue">Aal-Imran</span>
                 <h1>Quran Academy</h1>
               </h1>
-              <h4 className="text-xl text-left wleading-10 hidden lg:block w-3/4 leading-8">
+              <h4 className="text-xl md-text-left lg:text-left text-center mx-auto md:mx-0 lg:mx-0 block w-3/4 leading-8">
                 {
                   "We are a Quranic school where you can learn Qur'an, Arabic language and Islamic studies with a bunch of the best qualified instructors in all fields."
                 }
               </h4>
-              <div>
+              <div className="mx-auto md:mx-0 lg:mx-0">
                 <button
                   className="button | my-8 inline-flex items-center justify-center px-4 md:px-6 lg:px-6 py-2 text-blue hover:text-brown font-bold text-xl hover:text-main whitespace-no-wrap transition duration-200 ease-in-out bg-white rounded-md bg-main focus:outline-none max-w-sm"
                   id="submit-button"
@@ -85,14 +85,14 @@ export default function Home() {
                 </button>
               </div>
             </div>
-            {/* <div className="w-2/4">
+            <div className="w-2/4 hidden md:block lg:block">
               <Image
                 src={familyImage}
                 alt="Family"
                 id="blur"
-                className=" h-full object-cover bg-contain flex align-middle place-self-center"
+                className="h-full object-cover bg-contain flex align-middle place-self-center"
               />
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
