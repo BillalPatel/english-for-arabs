22ef1102c691f150b5cfc70d46876bc9648a6da8
Billal <billalpatel.bp@gmail.com>
2024-02-13 10:18:08 +0000

    Course cards

--- app/data/course/course.ts
+++ app/data/course/course.ts
@@ -2,31 +2,31 @@ export const courseData = [
   {
     name: "Foundation",
     description:
-      "In Qur'an foundation Course, you learn how to read: starting from the alphabet, until you can read Qur'an properly. This course qualifies you to the Tajweed course, with which you become a qualified Qari' inshaa Allah. Aal Imran Academy, is more than a family.",
+      "In Qur'an foundation Course, you learn how to read Quran, starting from the alphabet, until you can read Qur'an properly inshaa Allah. Time to take your first step!",
     image: "image_placeholder_foundation.jpg",
   },
   {
     name: "Reciting with Tajweed",
     description:
-      "It is a dream of each Muslim to recite the Quran as the Prophet Muhammad and his companions did. You can make this come true by taking your first step and joining us in Aal-Imran Academy, where you can learn Quran recitation in the best and easiest way inshaa Allah.",
+      "It is a dream of each Muslim to recite the Quran as the Prophet Muhammad and his companions did. Make it come true by joining Tajweed course now.",
     image: "image_placeholder_reciting.jpg",
   },
   {
     name: "Memorization",
     description:
-      "It will be said to the companion of the Qur'an: 'Recite and rise in status as you used to recite in the world, and your position will be at the last verse you recite. What a great thing! But how can we get that if we do not memorize the Qur'an? Well, simply join us in Aal-Imran Academy where you can memorize Quran simply and easily inshaa Allah.",
+      "It will be said to the companion of the Qur'an: 'Recite and rise in status as you used to recite in the world, and your position will be at the last verse you recite. Go for it!",
     image: "image_placeholder_memorization.jpg",
   },
   {
     name: "Ijazah",
     description:
-      "Imagine you are reciting Quran to the Prophet Muhammad Peace be upon him. WOW! This is really breathtaking. What if you have a connected chain (Sanad) to the Prophet Muhammad Peace be upon him? You can now have a connected chain to the Prophet Muhammad Peace be upon him by joining us in Aal-Imran Academy where you can start Ijazah Course now inshaa Allah.",
+      "Imagine you are reciting Quran to the Prophet Muhammad Peace be upon him. WOW! You can now a connected chain (Sanad) to the Prophet Muhammad PBUH.",
     image: "image_placeholder_ijazah.jpg",
   },
   {
     name: "10 Qira'at",
     description:
-      "In the 10 Qira'at course you will learn the most consecutive Quran recitations narrated about the Prophet Muhammad PBUH with qualified and certificated teachers.",
+      "In the 10 Qira'at  course you will learn the most consecutive Quran recitations narrated about the Prophet Muhammad PBUH with qualified and certificated teachers.",
     image: "image_placeholder_qiraat.jpg",
   },
 ];
