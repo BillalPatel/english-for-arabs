d6a1ace8d525a417fc99ca65da246b851c3eb465
Billal <billalpatel.bp@gmail.com>
2024-02-13 18:36:47 +0000

    Env vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,6 @@ const config: Config = {
       blue: "#035B73",
       brown: "#C2912E",
       gold: "#FEB601",
-      lightGold: "#F8C91F",
       darkGrey: "#A9A9A9",
       lightGrey: "#F4F4F4",
       black: "#11324D",
@@ -20,19 +19,6 @@ const config: Config = {
       red: "#EF4444",
       green: "#3CB371",
     },
-    //   // fontSize: {
-    //     // sm: "0.9375rem",
-    //     // 1: "1rem",
-    //     // "2xl": "1.5rem",
-    //     // "3xl": "1.75rem",
-    //     // "4xl": "2.5rem",
-    //     // "5xl": "4.5rem",
-    //   // },
-    //     // borderRadius: {
-    //       // default: 'rounded-2xl',
-    //       // large: "0.625rem",
-    //       // extraLarge: "0.625rem",
-    //     // },
   },
   plugins: [],
 };
